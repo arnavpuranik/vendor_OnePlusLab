1499ee577a958dbcdb870664fece24f615e68fae
LuK1337 <priv.luk@gmail.com>
2020-01-13 23:37:42 +0100

    TouchGestures: Switch to (ctx).getSystemService(x.class) everywhere
    Change-Id: I16ae2a1c92ced93a3e34c18d90e59ac5ca14c319

--- src/com/android/touch/gestures/KeyHandler.java
+++ src/com/android/touch/gestures/KeyHandler.java
@@ -98,21 +98,21 @@ public class KeyHandler implements DeviceKeyHandler {
     public KeyHandler(final Context context) {
         mContext = context;
 
-        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mAudioManager = mContext.getSystemService(AudioManager.class);
 
-        mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        mPowerManager = context.getSystemService(PowerManager.class);
         mGestureWakeLock = mPowerManager.newWakeLock(
                 PowerManager.PARTIAL_WAKE_LOCK, "TouchscreenGestureWakeLock");
 
         mEventHandler = new EventHandler();
 
-        mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mCameraManager = mContext.getSystemService(CameraManager.class);
         mCameraManager.registerTorchCallback(new TorchModeCallback(), mEventHandler);
 
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        mVibrator = context.getSystemService(Vibrator.class);
 
         if (mProximitySensor != null) {
-            mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+            mSensorManager = context.getSystemService(SensorManager.class);
             mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
             mProximityWakeLock = mPowerManager.newWakeLock(
                     PowerManager.PARTIAL_WAKE_LOCK, "TouchscreenGestureProximityWakeLock");
